c7e19acbb89466459fea56416ffef5856b240955
Ian Davies <github@iandavies.org>
2012-07-27 18:23:59 +0100

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject easel "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repo")))}
+  :repositories {"local" ~(str (.toURI (java.io.File. "repo")))}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [seesaw "1.4.2"]
-                 [jogl/jogl "0.0.1"]]
+                 [local/jogl.all.cl "1.0.0"]]
 
   :aot [easel.RunnableGraphicsAlgorithm
         easel.Renderer]
